130e856bee452722240f23fb5eca2dc0d998f26d
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2017-04-14 19:57:51 +0200

    Implement normal getter and setter

--- src/lib.rs
+++ src/lib.rs
@@ -5,22 +5,34 @@ pub trait Hasher {
 }
 
 // TODO: Eventually move to own module.
-pub struct Map<V: Default, H: Hasher> {
+pub struct Map<V, H> {
 	hash: H,
-	backing: Vec<V>,
+	backing: Box<[V]>,
 }
 
-impl<V: Default + Clone, H: Hasher> Map<V, H> {
+impl<V: Default, H: Hasher> Map<V, H> {
 	pub fn new(hash: H) -> Self {
 		let size = hash.size();
+		let mut vec : Vec<V> = Vec::with_capacity(size);
+		for _ in 0..size {
+			vec.push(V::default());
+		}
 		Map {
 			hash: hash,
-			backing: vec![V::default(); size],
+			backing: vec.into_boxed_slice(),
 		}
 	}
 
 	pub fn print_size(&self) {
 		println!("Size is {}.", self.hash.size());
+	}
+
+	pub fn insert(&mut self, k: H::K, v: V) {
+		self.backing[self.hash.hash(k)] = v;
+	}
+
+	pub fn get(&mut self, k: H::K) -> &V {
+		&self.backing[self.hash.hash(k)]
 	}
 }
 
@@ -53,8 +65,8 @@ fn it_works() {
 		}
 	};
 
-	let mut mymap : Map<String, _> = Map::new(Cuboid::new(10, 20, 30));
-	mymap.print_size(); /*
+	let mut mymap = Map::new(Cuboid::new(10, 20, 30));
+	mymap.print_size();
 	mymap.insert((0, 3, 7), "Hello ");
 	mymap.insert((4, 19, 13), "lovely");
 	mymap.insert((9, 8, 29), "World!");
@@ -62,5 +74,5 @@ fn it_works() {
 	print!("{}", mymap.get((2, 15, 2))); // ""
 	print!("{}", mymap.get((9, 8, 29))); // "World!"
 	print!("{}", mymap.get((7, 4, 23))); // ""
-	println!(); // */
+	println!();
 }
