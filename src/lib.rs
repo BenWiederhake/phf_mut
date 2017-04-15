39bf02f80e53283238fe14bb9552efeb43c59042
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2017-04-15 14:04:10 +0200

    Implement Debug, Index, IndexMut, show off in tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::fmt;
+use std::ops::{Index, IndexMut};
+
 pub trait Hasher {
 	type K;
 	fn hash(&self, k: Self::K) -> usize;
@@ -22,14 +25,59 @@ impl<V: Default, H: Hasher> Map<V, H> {
 			backing: vec.into_boxed_slice(),
 		}
 	}
+}
+
+impl<V, H: Hasher> Map<V, H> {
+	pub fn from_initial(hash: H, init: Vec<V>) -> Self {
+		let size = hash.size();
+		assert_eq!(size, init.len());
+		Map {
+			hash: hash,
+			backing: init.into_boxed_slice(),
+		}
+	}
 
 	pub fn insert(&mut self, k: H::K, v: V) {
 		self.backing[self.hash.hash(k)] = v;
 	}
 
-	pub fn get(&mut self, k: H::K) -> &V {
+	pub fn get(&self, k: H::K) -> &V {
 		&self.backing[self.hash.hash(k)]
 	}
+
+	pub fn get_mut(&mut self, k: H::K) -> &mut V {
+		&mut self.backing[self.hash.hash(k)]
+	}
+}
+
+impl<V, H> Map<V, H> {
+	pub fn is_empty(&self) -> bool {
+		self.backing.is_empty()
+	}
+
+	pub fn len(&self) -> usize {
+		self.backing.len()
+	}
+}
+
+impl<V, H> fmt::Debug for Map<V, H> where V: fmt::Debug {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        write!(fmt, "{:?}", &*self.backing)
+    }
+}
+
+impl<V, H: Hasher> Index<H::K> for Map<V, H> {
+    type Output = V;
+
+    fn index(&self, k: H::K) -> &V {
+        self.get(k)
+    }
+}
+
+impl<V, H: Hasher> IndexMut<H::K> for Map<V, H> {
+    fn index_mut(&mut self, k: H::K) -> &mut V {
+        self.get_mut(k)
+    }
 }
 
 #[test]
@@ -59,15 +107,16 @@ fn it_works() {
 		fn size(&self) -> usize {
 			self.hash((self.w - 1, self.h - 1, self.d - 1)) + 1
 		}
-	};
+	}
 
 	let mut mymap = Map::new(Cuboid::new(10, 20, 30));
-	mymap.insert((0, 3, 7), "Hello ");
-	mymap.insert((4, 19, 13), "lovely");
-	mymap.insert((9, 8, 29), "World!");
+	mymap.insert((0, 3, 7), "Hello ".to_string());
+	mymap[(0, 3, 7)].push(' ');
+	mymap.insert((4, 19, 13), "lovely".to_string());
+	mymap.insert((9, 8, 29), "World!".to_string());
 	print!("{}", mymap.get((0, 3, 7))); // "Hello "
 	print!("{}", mymap.get((2, 15, 2))); // ""
-	print!("{}", mymap.get((9, 8, 29))); // "World!"
+	print!("{}", mymap[(9, 8, 29)]); // "World!"
 	print!("{}", mymap.get((7, 4, 23))); // ""
 	println!();
 }
