f0608be1d8bc85d4c9f235ad61c08013c73ec651
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2017-04-17 11:31:55 +0200

    Tests: switch to more realistic use case

--- src/tests.rs
+++ src/tests.rs
@@ -18,42 +18,58 @@ use {Hasher, HasherInverse, Map, Set};
 
 /* === Example use case === */
 
-struct Cuboid {
-    w: usize,
-    h: usize,
-    d: usize,
+struct Pairs {
+    n: usize,
 }
 
-impl Cuboid {
-    pub fn new(w: usize, h: usize, d: usize) -> Self {
-        assert!(w > 0);
-        assert!(h > 0);
-        assert!(d > 0);
-        Cuboid { w: w, h: h, d: d }
+impl Pairs {
+    pub fn new(n: usize) -> Self {
+        Pairs { n: n }
+    }
+
+    fn sort((u, v): (usize, usize)) -> (usize, usize) {
+        if u > v {
+            (v, u)
+        } else {
+            (u, v)
+        }
+    }
+
+    fn size_when(n: usize) -> usize {
+        (n + 1) * n / 2
     }
 }
 
-impl Hasher for Cuboid {
-    type K = (usize, usize, usize);
+impl Hasher for Pairs {
+    type K = (usize, usize);
 
-    fn hash(&self, (x, y, z): Self::K) -> usize {
-        x + self.w * y + self.w * self.h * z
+    fn hash(&self, k: Self::K) -> usize {
+        let (a, b) = Self::sort(k);
+        a + Self::size_when(b)
     }
 
     fn size(&self) -> usize {
-        self.hash((self.w - 1, self.h - 1, self.d - 1)) + 1
+        Self::size_when(self.n)
     }
 }
 
-impl HasherInverse for Cuboid {
+impl HasherInverse for Pairs {
     fn invert(&self, index: usize) -> Self::K {
-        let x = index % self.w;
-        let index = index / self.w;
-        let y = index % self.h;
-        let index = index / self.h;
-        assert!(index < self.d);
-        let z = index;
-        (x, y, z)
+        assert!(index < self.size());
+        /* Count down from n-1 to 0. */
+        for sub in 0..self.n {
+            let use_b = self.n - 1 - sub;
+
+            let offset = Self::size_when(use_b);
+            if offset <= index {
+                /* Found the largest 'use_b' that results
+                   in a compatible offset */
+                let use_a = index - offset;
+                assert!(use_a <= use_b);
+                return (use_a, use_b);
+            }
+        }
+        panic!("Unsigned integer {} appears to be strictly smaller than zero 0.", index);
     }
 }
 
@@ -61,68 +77,81 @@ impl HasherInverse for Cuboid {
 
 #[test]
 fn test_map_basics() {
-    let mut mymap = Map::new(Cuboid::new(10, 20, 30));
-    mymap.insert((0, 3, 7), "Hello".to_string());
-    mymap[(0, 3, 7)].push(' ');
-    mymap.insert((4, 19, 13), "lovely".to_string());
-    mymap.insert((9, 8, 29), "World!".to_string());
-    assert_eq!("Hello ", mymap.get((0, 3, 7)));
-    assert_eq!("", mymap.get((2, 15, 2)));
-    assert_eq!("World!", mymap[(9, 8, 29)]);
-    assert_eq!("", mymap.get((7, 4, 23)));
+    let mut mymap = Map::new(Pairs::new(10));
+    mymap.insert((3, 7), String::from("Hello"));
+    mymap[(7, 3)].push(' ');
+    mymap.insert((4, 3), String::from("lovely"));
+    mymap.insert((2, 9), String::from("World!"));
+    assert_eq!("Hello ", mymap.get((3, 7)));
+    assert_eq!("Hello ", mymap.get((7, 3)));
+    assert_eq!("", mymap.get((2, 2)));
+    assert_eq!("World!", mymap[(2, 9)]);
+    assert_eq!("World!", mymap[(9, 2)]);
+    assert_eq!("", mymap.get((7, 4)));
+    assert_eq!("", mymap.get((6, 6)));
 }
 
 #[test]
 fn test_map_iter() {
-    let mut mymap = Map::new(Cuboid::new(2, 3, 2));
-    mymap.insert((0, 1, 0), 42);
-    mymap.insert((1, 0, 1), 44);
-    mymap.insert((1, 2, 0), 123);
-    mymap.insert((1, 1, 1), 0xCAFE);
-    mymap[(1, 0, 1)] = 5;
+    let mut mymap = Map::new(Pairs::new(3));
+    mymap.insert((0, 1), 42);
+    mymap.insert((1, 2), 123);
+    mymap.insert((1, 1), 0xCAFE);
+    mymap[(1, 0)] = 5;
     let value_vec = mymap.values().map(|x| *x).collect::<Vec<_>>();
-    assert_eq!(vec![0, 0, 42, 0, 0, 123, 0, 5, 0, 0xCAFE, 0, 0], value_vec);
-    let entry_vec = mymap.iter().map(|((x, y, z), &v)| (x, y, z, v))
+    assert_eq!(vec![0, 5, 0xCAFE, 0, 123, 0], value_vec);
+    let entry_vec = mymap.iter().map(|((a, b), &v)| (a, b, v))
                          .collect::<Vec<_>>();
-    assert_eq!(vec![(0, 0, 0, 0), (1, 0, 0, 0),
-                    (0, 1, 0, 42), (1, 1, 0, 0),
-                    (0, 2, 0, 0), (1, 2, 0, 123),
-                    (0, 0, 1, 0), (1, 0, 1, 5),
-                    (0, 1, 1, 0), (1, 1, 1, 0xCAFE),
-                    (0, 2, 1, 0), (1, 2, 1, 0),], entry_vec);
+    assert_eq!(vec![(0, 0, 0), (0, 1, 5),
+                    (1, 1, 0xCAFE), (0, 2, 0),
+                    (1, 2, 123), (2, 2, 0),], entry_vec);
 }
 
 /* === Actual tests: Set === */
 
 #[test]
 fn test_set_basics() {
-    let mut myset = Set::new(Cuboid::new(10, 20, 30));
-    myset.insert((7, 6, 5));
-    myset.insert((4, 3, 2));
-    myset.insert((1, 0, 8));
-    assert_eq!(true, myset.contains((7, 6, 5)));
-    assert_eq!(false, myset.contains((7, 8, 9)));
-    assert_eq!(true, myset.contains((4, 3, 2)));
-    assert_eq!(false, myset.contains((9, 10, 11)));
-    assert_eq!(true, myset.contains((1, 0, 8)));
-    assert_eq!(false, myset.contains((5, 15, 25)));
+    let mut myset = Set::new(Pairs::new(10));
+    myset.insert((7, 6));
+    myset.insert((4, 3));
+    assert_eq!(true, myset.contains((7, 6)));
+    assert_eq!(true, myset.contains((6, 7)));
+    assert_eq!(false, myset.contains((7, 8)));
+    assert_eq!(true, myset.contains((4, 3)));
+    assert_eq!(false, myset.contains((9, 8)));
+    assert_eq!(false, myset.contains((1, 0)));
+    assert_eq!(false, myset.contains((0, 1)));
+    assert_eq!(false, myset.contains((5, 5)));
 
-    myset.erase((4, 3, 2));
-    assert_eq!(true, myset.contains((7, 6, 5)));
-    assert_eq!(false, myset.contains((7, 8, 9)));
-    assert_eq!(false, myset.contains((4, 3, 2))); /* Change */
-    assert_eq!(false, myset.contains((9, 10, 11)));
-    assert_eq!(true, myset.contains((1, 0, 8)));
-    assert_eq!(false, myset.contains((5, 15, 25)));
+    myset.insert((1, 0));
+    assert_eq!(true, myset.contains((7, 6)));
+    assert_eq!(true, myset.contains((6, 7)));
+    assert_eq!(false, myset.contains((7, 8)));
+    assert_eq!(true, myset.contains((4, 3)));
+    assert_eq!(false, myset.contains((9, 8)));
+    assert_eq!(true, myset.contains((1, 0))); /* Change */
+    assert_eq!(true, myset.contains((0, 1))); /* Change */
+    assert_eq!(false, myset.contains((5, 5)));
+
+    myset.erase((4, 3));
+    assert_eq!(true, myset.contains((7, 6)));
+    assert_eq!(true, myset.contains((6, 7)));
+    assert_eq!(false, myset.contains((7, 8)));
+    assert_eq!(false, myset.contains((4, 3))); /* Change */
+    assert_eq!(false, myset.contains((9, 8)));
+    assert_eq!(true, myset.contains((1, 0)));
+    assert_eq!(true, myset.contains((0, 1)));
+    assert_eq!(false, myset.contains((5, 5)));
 }
 
 #[test]
 fn test_set_iter() {
-    let mut myset = Set::new(Cuboid::new(10, 20, 30));
-    myset.insert((7, 6, 5));
-    myset.insert((4, 3, 2));
-    myset.insert((1, 0, 8));
+    let mut myset = Set::new(Pairs::new(10));
+    myset.insert((7, 6));
+    myset.insert((4, 3));
+    myset.insert((1, 0));
+    myset.insert((1, 4));
     let myset: Set<_> = myset;
     let as_vec = myset.iter().collect::<Vec<_>>();
-    assert_eq!(vec![(4, 3, 2), (7, 6, 5), (1, 0, 8)], as_vec);
+    assert_eq!(vec![(0, 1), (1, 4), (3, 4), (6, 7)], as_vec);
 }
